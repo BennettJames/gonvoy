31fc78638c94709ba324d146c14a7f0e05cb835d
BennettJames <bennett.sala@gmail.com>
2022-10-29 19:15:06 -0700

    Initial test

---
+++ internal/containertest/scratch_test.go
@@ -0,0 +1,77 @@
+package containertest
+
+import (
+	"context"
+	"fmt"
+	"net/http"
+	"testing"
+
+	"github.com/testcontainers/testcontainers-go"
+	"github.com/testcontainers/testcontainers-go/wait"
+)
+
+func TestEnvoy(t *testing.T) {
+	if testing.Short() {
+		t.Skip("skipping integration test")
+	}
+
+	ctx := context.Background()
+	var _ = ctx
+
+}
+
+func TestWithNginx(t *testing.T) {
+	if testing.Short() {
+		t.Skip("skipping integration test")
+	}
+
+	ctx := context.Background()
+
+	nginxC, err := setupNginx(ctx)
+	if err != nil {
+
+		t.Fatal(err)
+	}
+
+	// Clean up the container after the test is complete
+	defer nginxC.Terminate(ctx)
+
+	resp, err := http.Get(nginxC.URI)
+	if resp.StatusCode != http.StatusOK {
+		t.Fatalf("Expected status code %d. Got %d.", http.StatusOK, resp.StatusCode)
+	}
+}
+
+type nginxContainer struct {
+	testcontainers.Container
+	URI string
+}
+
+func setupNginx(ctx context.Context) (*nginxContainer, error) {
+	req := testcontainers.ContainerRequest{
+		Image:        "nginx",
+		ExposedPorts: []string{"80/tcp"},
+		WaitingFor:   wait.ForHTTP("/"),
+	}
+	container, err := testcontainers.GenericContainer(ctx, testcontainers.GenericContainerRequest{
+		ContainerRequest: req,
+		Started:          true,
+	})
+	if err != nil {
+		return nil, fmt.Errorf("Could not create container: %s", err)
+	}
+
+	ip, err := container.Host(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	mappedPort, err := container.MappedPort(ctx, "80")
+	if err != nil {
+		return nil, err
+	}
+
+	uri := fmt.Sprintf("http://%s:%s", ip, mappedPort.Port())
+
+	return &nginxContainer{Container: container, URI: uri}, nil
+}
